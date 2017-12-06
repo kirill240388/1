50b560b8036988fee5ac62feb56c3958bda2019e
федоровк <@227_8>
2017-12-06 09:38:47 +0600

    Add project files.

---
+++ ConsoleApplication11/Source.cpp
@@ -0,0 +1,25 @@
+#include<stdlib.h>
+#include<iostream>
+#include<locale.h>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int x;
+	cin >> x;
+	switch (x)
+	{
+	case 6:
+	{
+		cout << "x=6";
+	}
+
+	break;
+	default
+
+
+
+
+	system("pause");
+}
